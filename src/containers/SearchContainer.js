e18f740d6600747a9347c418780b8f0d0aeb75aa
Emma Li <emma.m.li.98@gmail.com>
2020-10-03 13:39:12 -0700

    Implements container components using .connect

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,8 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
 
-var SearchContainer = () => {};
+const mapDispatchToProps = (dispatch) => ({
+  getYouTubeVideos: (q) => dispatch(handleSearchChange(q))
+});
 
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
-
-export default SearchContainer;
+export default connect(null, mapDispatchToProps)(Search);
