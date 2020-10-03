690c8f9e22f901282b36eb1b48bea249c5c88fa3
Emma Li <emma.m.li.98@gmail.com>
2020-10-03 13:12:49 -0700

    Configures the store in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,15 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-// TODO:  Create your redux store, apply thunk as a middleware, and export it!
+const initialState = {
+  currentVideo: exampleVideoData[0],
+  videoList: exampleVideoData
+};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(thunk)
+);
+
+export default store;
